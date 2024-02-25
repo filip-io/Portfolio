2e46c12500a413fb6c63917f1fce1445ac480248
Filip <79668005+filip-io@users.noreply.github.com>
2024-02-25 23:56:25 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const filipImage = document.getElementById("filip");
+filipImage.addEventListener("click", function () {
+    const body = document.body;
+    if (body.style.backgroundColor === "rgb(12, 11, 37)") {
+        body.style.backgroundColor = "black";
+    } else {
+        body.style.backgroundColor = "rgb(12, 11, 37)";
+    }
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let typedCharacters = '';
+    const magicKeyword = 'magic';
+
+    function handleMagicWord(event) {
+
+        typedCharacters += event.key.toLowerCase();
+
+        if (typedCharacters === magicKeyword) {
+
+            const modal = document.getElementById('dennis');
+            modal.style.display = 'block';
+
+            typedCharacters = '';
+        } else if (!magicKeyword.startsWith(typedCharacters)) {
+
+            typedCharacters = '';
+        }
+    }
+
+
+    document.addEventListener('keydown', handleMagicWord);
+
+    const modal = document.getElementById('dennis');
+    modal.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    const modalCloseBtn = document.querySelector('.modal-close');
+    modalCloseBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+});
+
+
+
+
